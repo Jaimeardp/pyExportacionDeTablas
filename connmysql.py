8d1f7db1dcb2f2cc5109101c65997fbfe197957c
Jaimeardp <jaimeardp@gmail.com>
2017-08-17 05:05:50 -0500

    Exportar + JSON 17/08/17 5:05 AM

--- connmysql.py
+++ connmysql.py
@@ -2,7 +2,7 @@ import pymysql
 from pandas import DataFrame, Series
 import pandas as pd
 import numpy
-
+import json
 class Connection(object):
     """docstring for Connection"""
 
@@ -38,10 +38,8 @@ class Connection(object):
         cursor.execute(query)
         results = cursor.fetchall()
         print(type(results))
-        #print(results)
         l = list(results)
         frame = DataFrame(l)
-        #print(frame[:10])
         arr = open('nuevo.txt','w')
         for l in range(0,len(frame)):
             dic[l] = []
@@ -52,8 +50,24 @@ class Connection(object):
 
         return dic
 
+
     def json(self,nom):
-        pass
+        db = self._Con()
+        cursor =  db.cursor()
+        query = 'select * from '+nom
+        cursor.execute(query)
+        results = cursor.fetchall()
+        print(type(results))
+        l = list(results)
+        frame = DataFrame(l)
+        js = frame.to_json(orient="records")
+        print(type(js))
+        print(js)
+        data = json.loads(js)
+        with open('nuevo1.json','w') as outfile:
+            json.dump(data,outfile)
+
+        return js
 
                   
 #?INDICES DEL FRAME NO UTILIZADO
@@ -62,12 +76,14 @@ class Connection(object):
 
 con = Connection('root','mysql','prestocash')
 
-#con.mostrar_tbl('tb_cliente')
+con.mostrar_tbl('tb_estado_articulo')
 
-di= con.txt('tb_cambio')
+#di= con.txt('tb_cambio')
 
-print(di)
+#print(di)
 
+js = con.json('tb_estado_articulo')
+print(js)
 
 
 
