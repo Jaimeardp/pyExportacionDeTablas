508a3cef8ed032d4584e03aea955da2f2574f9f2
Jaimeardp <jaimeardp@gmail.com>
2017-08-17 20:52:56 -0500

    Bulding Program v.2 17/08/17 20:50 PM

--- Parser.py
+++ Parser.py
@@ -1,14 +1,38 @@
 import argparse
+from conmysql import *
+
+global nomTabla
+
+
+class ViewTableWithFormat(object):
+	"""docstring for ViewTableWithFormat"""
+	def __init__(self, arg):
+		super(ViewTableWithFormat, self).__init__()
+		self.arg = arg
+		
+
+
+
+
+
+
+
+###### MAIN ######
 
 parser = argparse.ArgumentParser(description='Mi Primer Hola Mundo')
 #parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer for the accumulator')
 #parser.add_argument('-s','--sum', dest='accumulate', action='store_const',const = sum, default= max , help='sum the integers (default: find the max)')
-parser.add_argument("-js","--json", help="Target URL and IP Address\n  > e.g -t 127.0.0.1")
-
+parser.add_argument("-js","--exjson", help="Ingrese tabla para exportar en formato json \n > e.g tbl_name")
+parser.add_argument("-txt","--extxt", help="Ingrese tabla para exportar en formato txt \n > e.g tbl_name")
 args = parser.parse_args()
 
 print('WHAT SAID ',args.json)
 print(type(args.json))
+
+if args.exjson:
+	nomTabla = args.exjson
+if args.extxt:
+	nomTabla = args.extxt
 
 #if args.target:
 #	print('PRUEBA')
